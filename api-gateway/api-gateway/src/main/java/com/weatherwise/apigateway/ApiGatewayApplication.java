46885bdf7c968e8505871cdcb8c4df1509b3bba3
Mattia Piazzalunga <mattiapiazzalunga@outlook.com>
2024-01-12 05:49:27 +0100

    some updates

--- api-gateway/api-gateway/src/main/java/com/weatherwise/apigateway/ApiGatewayApplication.java
+++ api-gateway/api-gateway/src/main/java/com/weatherwise/apigateway/ApiGatewayApplication.java
@@ -2,9 +2,11 @@ package com.weatherwise.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableHystrix
 public class ApiGatewayApplication {
 
